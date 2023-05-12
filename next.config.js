009800992d14be5b4486ccbdfe09995e7e9c84ee
Jason Chong <jasoncwy@hotmail.com>
2023-05-11 20:05:35 -0400

    Add file structure

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+	// ...
+	webpackDevMiddleware: (config) => {
+		config.watchOptions = {
+			poll: 1000, // Check for changes every second
+			aggregateTimeout: 300, // delay before rebuilding
+		};
+		return config;
+	},
+	// ...
+};
